a0ff9406ddc0cd3a311fd900344c426f7ee3ab31
USER <mac@USERs-MacBook-Pro.local>
2025-03-28 14:20:34 +0100

    used cors

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 import 'dotenv/config'
 import express from 'express'
 import { DB_connection } from './dbconfig.js'
+import cors from 'cors'  // Import cors
 import routes from './router.js'
 const app = express();
+
+app.use(cors({ origin: 'http://localhost:5173' })); 
+
 app.use(express.json());
 const port = process.env.PORT || 800
 
