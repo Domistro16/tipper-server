bb36744771a53cc90b46af7cc50aea3b663c1ef6
USER <mac@USERs-MacBook-Pro.local>
2025-04-04 00:13:27 +0100

    change

--- router.js
+++ router.js
@@ -162,8 +162,10 @@ router.get("/droptips/rel/", async (req, res) => {
 router.post("/nft/upload", upload.single("file"), async (req, res) =>{
     try {
         console.log("File received:", req.file);
-        if (!req.file) return res.status(400).json({ error: "No file uploaded" });
-        const upload = await pinata.upload.public.file(req.file.buffer);
+        if (!req.file) return res.status(400).json({ error: "No file uploaded" })
+        const formData = new FormData();
+        formData.append("file", req.file.buffer, { filename: req.file.originalname });;
+        const upload = await pinata.upload.public.file(formData);
         url = "https://gateway.pinata.cloud/ipfs/" + upload.cid
         res.status(200).json({message: 'Files uploaded successfully', url: url})
     }catch(error){
