c1ab6c0aefa6ab81e7ece45f08854ce1622c761b
Desmond Egwurube <desmondesih@gmail.com>
2025-06-23 16:12:14 +0100

    added verify hash

--- mintQueue.js
+++ mintQueue.js
@@ -10,9 +10,9 @@ const redisOpts = {
   host: process.env.REDIS_INTERNAL_HOST || "127.0.0.1",
   port: Number(process.env.REDIS_PORT) || 6379,
 };
-const redisClient = new Redis(redisOpts);
+const redisClient = new Redis(process.env.REDIS_INTERNAL_HOST);
 
-const mintQueue = new Queue("mintQueue", { redis: redisOpts });
+const mintQueue = new Queue("mintQueue", { redis: process.env.REDIS_INTERNAL_HOST });
 
 // ─── Exported helpers ───────────────────────────────────────────────────────────
 
