611586fed44c8d01dbdecced2635c1a98a729f31
Desmond Egwurube <desmondesih@gmail.com>
2025-10-09 13:41:10 +0100

    Updated courses

--- dbconfig.js
+++ dbconfig.js
@@ -1,13 +1,13 @@
-import mongoose from 'mongoose';
+import mongoose from "mongoose";
 
 export const DB_connection = async () => {
-  
   try {
-    mongoose.set('strictQuery', false);
-    const conn = await mongoose.connect(process.env.CONNECTION_STRING);
+    mongoose.set("strictQuery", false);
+    const conn = await mongoose.connect(
+      process.env.CONNECTION_STRING || process.env.MONGO_URL
+    );
     console.log(`MongoDB connection success on : ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
   }
-
-}
+};
